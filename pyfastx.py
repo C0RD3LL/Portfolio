5836cf370ca20456b2305c7d910508a4ba6a3ec8
Cordell Browne <88411627+C0RD3LL@users.noreply.github.com>
2022-01-05 13:33:27 -0500

    Add files via upload

---
+++ pyfastx.py
@@ -0,0 +1,35 @@
+#Thanks for looking at my portfolio 
+# ~Cordell Browne
+###########Imports###########
+import os
+import gzip
+import sys
+from datetime import datetime
+import pyfastx
+import psutil
+#############################
+###########Help##############
+
+if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+    print(" \n ¯\_(ツ)_/¯ \n") 
+    quit() 
+
+#############################
+
+startTime = datetime.now()
+
+fastq = sys.argv[1]
+
+fq = pyfastx.Fastq(fastq)
+
+#Get memory used in GBs 
+pid = os.getpid()
+python_process = psutil.Process(pid)
+memoryUse = python_process.memory_info()[0]/2.**30  
+
+
+print("Total Reads  :"len(fq))
+print("Total Bases  :"fq.size)
+print("Base per Read:"fq.avglen)
+print("Runtime      :" datetime.now() - startTime)
+print('memory use   :', memoryUse)
